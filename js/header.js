b4a6cb6a7cfce2eeef154f0b1293fda5c8554fc6
arielle-m <ariem.marii@gmail.com>
2024-08-20 23:07:54 -0700

    edited transition point and styled logo in nav onscroll

--- js/header.js
+++ js/header.js
@@ -1,14 +1,16 @@
 const header = document.getElementById( "masthead" );
 // console.log("waow");
+const heroLogoBottom = document.querySelectorAll("header.hero a.custom-logo-link img.custom-logo")[0].getBoundingClientRect().bottom;
 
+console.log(heroLogoBottom);
 // const headerDistanceToTop = header.getBoundingClientRect().bottom;
 // console.log(headerDistanceToTop);
 
 window.addEventListener( "scroll", () => {
     // console.log(window.scrollY);
-    if ( window.scrollY >= header.offsetHeight ) {
+    if ( window.scrollY >= heroLogoBottom ) {
         header.classList.add( "scrolled" );
-    } else if( window.scrollY < header.offsetHeight ) {
+    } else if( window.scrollY < heroLogoBottom ) {
         header.classList.remove ( "scrolled" );
     }
 });

a23959305e209084e2bb06778cb84e2bfa8c8a04
arielle-m <ariem.marii@gmail.com>
2024-09-11 19:18:28 -0700

    styled the header and navigation

--- header.php
+++ header.php
@@ -25,49 +25,51 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'enable-cookies' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<header id="masthead" class="site-header full-bleed">
+		<div>
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"
-			aria-label="Menu Toggle">
-				<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
-					<title>Menu icon</title>
-					<path d="M2 6h20"></path>
-					<path d="M2 12h20"></path>
-					<path d="M2 18h20"></path>
-				</svg>
-			</button>
-			<div class="menu-header-menu-container">
-				<ul id="header-menu" class="menu nav-menu">
-					<li id="menu-item-home" class="menu-item menu-item-post_type menu-item-object-page menu-item-home">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Home</a>
-					</li>
-					<li id="menu-item-cookies" class="menu-item menu-item-post_type menu-item-object-page menu-item-cookies">
-						<a href="#cookies">Cookies</a>
-					</li>
-					<li id="menu-item-packs" class="menu-item menu-item-post_type menu-item-object-page menu-item-packs">
-						<a href="#packs">Packs</a>
-					</li>
-					<li id="menu-item-about" class="menu-item menu-item-post_type menu-item-object-page menu-item-about">
-						<a href="#about">About</a>
-					</li>
-					<li id="menu-item-order" class="menu-item menu-item-post_type menu-item-object-page menu-item-order">
-						<a href="#order">Order</a>
-					</li>
-				</ul>
-			</div>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"
+				aria-label="Menu Toggle">
+					<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+						<title>Menu icon</title>
+						<path d="M2 6h20"></path>
+						<path d="M2 12h20"></path>
+						<path d="M2 18h20"></path>
+					</svg>
+				</button>
+				<div class="menu-header-menu-container">
+					<ul id="header-menu" class="menu nav-menu">
+						<li id="menu-item-home" class="menu-item menu-item-post_type menu-item-object-page menu-item-home">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Home</a>
+						</li>
+						<li id="menu-item-cookies" class="menu-item menu-item-post_type menu-item-object-page menu-item-cookies">
+							<a href="#cookies">Cookies</a>
+						</li>
+						<li id="menu-item-packs" class="menu-item menu-item-post_type menu-item-object-page menu-item-packs">
+							<a href="#packs">Packs</a>
+						</li>
+						<li id="menu-item-about" class="menu-item menu-item-post_type menu-item-object-page menu-item-about">
+							<a href="#about">About</a>
+						</li>
+						<li id="menu-item-order" class="menu-item menu-item-post_type menu-item-object-page menu-item-order">
+							<a href="#order">Order</a>
+						</li>
+					</ul>
+				</div>
+			</nav><!-- #site-navigation -->
+		</div>
 	</header><!-- #masthead -->

e0953658c6bd1e41587330a774d1fd52c2862b3a
arielle-m <ariem.marii@gmail.com>
2024-09-18 20:33:15 -0700

    fixed site logo on product pages and added right margins

--- js/header.js
+++ js/header.js
@@ -1,16 +1,18 @@
 const header = document.getElementById( "masthead" );
-// console.log("waow");
-const heroLogoBottom = document.querySelectorAll("header.hero a.custom-logo-link img.custom-logo")[0].getBoundingClientRect().bottom;
+const heroLogo = document.querySelectorAll("header.hero a.custom-logo-link img.custom-logo")[0];
+console.log(heroLogo);
+// console.log(heroLogoBottom);
 
-console.log(heroLogoBottom);
-// const headerDistanceToTop = header.getBoundingClientRect().bottom;
-// console.log(headerDistanceToTop);
-
-window.addEventListener( "scroll", () => {
-    // console.log(window.scrollY);
-    if ( window.scrollY >= heroLogoBottom ) {
-        header.classList.add( "scrolled" );
-    } else if( window.scrollY < heroLogoBottom ) {
-        header.classList.remove ( "scrolled" );
-    }
-});
+if (heroLogo == undefined) {
+    header.classList.add( "scrolled" );
+} else {
+    window.addEventListener( "scroll", () => {
+        const heroLogoBottom = heroLogo.getBoundingClientRect().bottom;
+        // console.log(window.scrollY);
+        if ( window.scrollY >= heroLogoBottom ) {
+            header.classList.add( "scrolled" );
+        } else if( window.scrollY < heroLogoBottom ) {
+            header.classList.remove( "scrolled" );
+        }
+    });
+}

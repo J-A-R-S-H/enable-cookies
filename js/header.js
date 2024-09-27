ca860f44b23a691c78100cfa7d5f672e3c41b7a4
arielle-m <ariem.marii@gmail.com>
2024-09-26 18:59:55 -0700

    prevented logo in hero and footer from being clickable

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 const header = document.getElementById( "masthead" );
 const heroLogo = document.querySelectorAll("header.hero a.custom-logo-link img.custom-logo")[0];
-console.log(heroLogo);
+// console.log(heroLogo);
 // console.log(heroLogoBottom);
 
 if (heroLogo == undefined) {
@@ -15,4 +15,22 @@ if (heroLogo == undefined) {
             header.classList.remove( "scrolled" );
         }
     });
+}
+
+
+const heroLogoLink = document.querySelectorAll("header.hero a.custom-logo-link")[0];
+if (heroLogoLink !== undefined) {
+    heroLogoLink.addEventListener('click', function (e) {
+        e.preventDefault();
+    } );
+    heroLogoLink.setAttribute('tabindex', '-1');
+}
+
+const footerLogoLink = document.querySelectorAll("footer a.custom-logo-link")[0];
+if (footerLogoLink !== undefined) {
+    footerLogoLink.addEventListener('click', function (e) {
+        e.preventDefault();
+    } );
+    
+    footerLogoLink.setAttribute('tabindex', '-1');
 }
